31ae13688976c1809a4273565b6156253811a082
Khalil <khalilhall1221@gmail.com>
2020-02-18 22:59:42 -0500

    attempting to deploy to heroku

---
+++ public/js/app.js
@@ -0,0 +1,58 @@
+
+$("#add-burger").on("click", function (event) {
+    
+    console.log("clicked");
+    event.preventDefault();
+    let burgerarray = [];
+    let newBurger = $("#burger_name").val().trim();
+    let burgersHere = $(".burgers-here");
+    let burgerdiv = $("<div>").addClass("burgerdiv");
+    let name = $("<h1>");
+    let burgerbutton = $("<button>").addClass("devour");
+    name.text(newBurger);
+    burgerbutton.text("Devour Now?")
+
+    console.log(newBurger);
+
+    burgerdiv.append(name, burgerbutton);
+    burgerarray.push(newBurger);
+    console.log(burgerarray);
+    burgersHere.append(burgerdiv);
+
+    let burgerBuild = {
+        burger_name: $("#burger_name").val().trim(),
+        devoured : 0
+    }
+
+    $.post("/api/burgers", burgerBuild)
+        .then(function (data) {
+            console.log(data);
+        })
+    $("#burger_name").val("");
+})
+
+$(".devour").on("click", function(event){
+    event.preventDefault();
+    var id = $(this).data("id");
+    var newdevoured = $(this).data("1");
+
+    var Eaten = {
+      devoured: newdevoured
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: Eaten
+    }).then(
+      function() {
+        console.log("changed devoured to", newdevoured);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+
+})
+
+
+
--- server.js
+++ server.js
@@ -7,44 +7,50 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+  connection = mysql.createConnection({
     host: "127.0.0.1",
     port: 8080,
     user: "root",
     password: "root",
     database: "burgers_db"
-});
+  });
+};
 
-connection.connect(function(err){
-    if(err){
-        console.log("error connecting", +err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+  if (err) {
+    console.log("error connecting", +err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // Serve index.handlebars to the root route.
-app.get("/", function(req, res) {
-    connection.query("SELECT * FROM burger;", function(err, data) {
-      if (err) {
-        console.log(err)
-      }
-  
-      res.render("index", { burger: data });
-    });
+app.get("/", function (req, res) {
+  connection.query("SELECT * FROM burger;", function (err, data) {
+    if (err) {
+      console.log(err)
+    }
+
+    res.render("index", { burger: data });
   });
+});
 
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
-  
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
